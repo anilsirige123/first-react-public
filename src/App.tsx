4a5c521b099e58e3aaf6a6463b75ddd4aee5fcf1
anil <anil.sirige@thrymr.net>
2024-08-22 17:46:50 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import ListGroup from "./components/ListGroup"
 
 function App() {
   // below jsx 
-  return <div><ListGroup /></div>
+  return <div>
+      <ListGroup />
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </div>
 }
 
 export default App
